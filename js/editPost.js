325bf03a85c9468a2f525360231b1e4e6f89e3e9
Elias <eliaspaulinoandrade@gmail.com>
2017-02-09 14:12:23 -0300

    comecando post

--- js/editPost.js
+++ js/editPost.js
@@ -2,9 +2,7 @@ $("#newPostEditor").click(function(){
     addEditPost(createEditPost(selecPostColor()), $(".left").first());
 });
 function addEditPost(post, local){
-    $("<div>kkkk</div>",{
-        "class":"post"
-    });
+    
 }
 function createEditPost(color){
 
